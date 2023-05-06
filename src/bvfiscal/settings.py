ccfcb5c90514efaed068f55ce379e402a49fa144
Bruno Silva <brunobritorj@outlook.com>
2023-05-06 20:10:27 +0200

    Creating AAD integration

--- src/bvfiscal/settings.py
+++ src/bvfiscal/settings.py
@@ -139,3 +139,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTHENTICATION_BACKENDS = [
+    'django_azure_ad_auth.backends.AzureAdAuthenticationBackend',
+]
